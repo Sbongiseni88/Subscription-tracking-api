160fa0375109b8598f9494d57f5bb8ad146bb16c
Sibongiseni <sidubazjhb024@student.wethinkcode.co.za>
2025-10-09 10:48:36 +0200

    added featured error handling to allow our application to be more scalable

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import {PORT} from './config/env.js';
-
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import subRouter from './routes/subscription.route.js';
 import authRouter from './routes/auth.routes.js';
 import connectDB from './Database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+
 
 const app= express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subRouter);
 app.use('/api/v1/auth', authRouter);
+
+app.use(errorMiddleware);
 
 
 const body='Welcome to Subscription tracking API';
